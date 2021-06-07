0a3e48755fdc2b08fdc9b131601727997fe48d9f
Tomas Borcin <tomas.borcin@espressif.com>
2021-06-07 10:26:49 +0200

    minor update

--- sync_issues_to_jira/manage_push_event.py
+++ sync_issues_to_jira/manage_push_event.py
@@ -1,9 +1,5 @@
-from jira import JIRA
 from github import Github
 import os
-import sys
-import json
-from sync_to_jira import *
 
 def check_push_event(event):
     github = Github(os.environ['GITHUB_TOKEN'])
--- sync_issues_to_jira/sync_to_jira.py
+++ sync_issues_to_jira/sync_to_jira.py
@@ -21,7 +21,7 @@ import sys
 import json
 from sync_pr import sync_remain_prs
 from sync_issue import *
-from manage_push_event import *
+from manage_push_event import check_push_event
 
 
 class _JIRA(JIRA):
