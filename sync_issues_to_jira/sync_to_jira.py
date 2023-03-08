173c062ebbf68aebe288dab2aefb84d56883c78b
Tomas Borcin <tomas.borcin@espressif.com>
2023-03-08 16:50:15 +0100

    Test

--- sync_issues_to_jira/sync_to_jira.py
+++ sync_issues_to_jira/sync_to_jira.py
@@ -19,6 +19,7 @@ from github import Github
 import os
 import sys
 import json
+import re
 from sync_pr import sync_remain_prs
 from sync_issue import *
 
@@ -52,14 +53,16 @@ def main():
         print(json.dumps(event, indent=4))
 
     event_name = os.environ['GITHUB_EVENT_NAME']  # The name of the webhook event that triggered the workflow.
-    print(f'{event = }')
     print(f'{event_name = }')
     input_action = event['inputs']['action']
     print(f'input action: {input_action}')
     if event_name == 'workflow_dispatch':
-        event['action'] = event['inputs']['action']
-        issue_numbers = event['inputs']['issue-numbers']
+        if input_action == 'mirror issues':
+            issue_numbers = event['inputs']['issue-numbers']
+            issues = re.split('\W+', issue_numbers)
     action = event["action"]
+    print(f'{event = }')
+    print(f'{issues = }')
     print(f'{action = }')
     print(f'{issue_numbers = }')
 
